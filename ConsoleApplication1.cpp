8e66404876cfee1ca114e3462170a30ba5ab77fa
PermakovSasha <userworkhom@gmail.com>
2024-11-05 20:03:16 +0200

    change code C++

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 int main() {
 	double x = 1.5;
-	double y = 2.5;
+	double y = 5.5;
 	double h = 1.0;
 	double Y;
 	cout << " * ******While * ******* \n" << endl;
-	while (x <= 2.5) {
-		Y = (x * x * x) + 3 * (pow(sin(x), 2)) - 3;
+	while (x <= 5.5) {
+		Y = (x * x * x*x) + 3 * (pow(sin(x), 2)) - 3;
 		cout << " x= " << x << ", Y= " << Y << endl;
 		x += h;
 	}
